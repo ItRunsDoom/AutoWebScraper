cf025065750444c776cea2d7e5855ad91d9ac612
Leon Katz <lgoldner@ringmountain.org>
2017-12-21 11:38:20 -0800

    This is the original

---
+++ main.py
@@ -0,0 +1,33 @@
+# Main function, makes program loop and handles input
+# Libraries neccesary. bs4 and requests
+# URL uses beautifulsoup4 to scrape and orginize the searched URL
+def URL():
+     from bs4 import BeautifulSoup
+     import requests
+     print("Please enter a URL you would like to read/scrape")
+     userInput = input("cmd: ")
+     # Uses requests to "request" a webpage. prettifys it, and prints it
+     page = requests.get(userInput)
+     soup = BeautifulSoup(page.content, 'html.parser')
+     print(soup.prettify())
+#tagSearch searches tags in the body as per user input
+def tagSearch():
+    print("Please enter an HTML tag you would like to search for")
+    userInput = input("cmd: ").lower
+    for userInput in soup:
+        print
+def main():
+    print("Welcome to the small automated web scraper 1.0 \n If you need help type 'help'")
+    # Program loop. The prompt is cmd:
+    while (True):
+        userInput = input("cmd: ").lower()
+        if (userInput == "help"):
+            print("Commands\n<help> brings this up\n<quit> quits the program\n<tag> lets you search for a specific HTML tag\n<url> lets you choose which URL to scrape, or input a 'URL Number'")
+        if (userInput == "quit"):
+            return False
+        if (userInput == "url"):
+            URL()
+        if (userInput == "tag"):
+            tagSearch()
+
+main()
